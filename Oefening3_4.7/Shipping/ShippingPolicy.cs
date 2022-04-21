255810e1e70e33587f11a7882bd65c8e1e013d5a
Peter Windemuller <peter.windemuller@nslogin.nl>
2022-04-21 17:03:26 +0200

    Oefening3_4.7

---
+++ Oefening3_4.7/Shipping/ShippingPolicy.cs
@@ -0,0 +1,32 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System.Threading.Tasks;
+
+namespace Shipping
+{
+    class ShippingPolicy : Saga<ShippingPolicyData>,
+        IAmStartedByMessages<OrderBilled>
+    {
+        static ILog log = LogManager.GetLogger<ShippingPolicy>();
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<ShippingPolicyData> mapper)
+        {
+            mapper.MapSaga(sagaData => sagaData.OrderId)
+                .ToMessage<OrderBilled>(message => message.OrderId);
+        }
+
+        public Task Handle(OrderBilled message, IMessageHandlerContext context)
+        {
+            log.Info($"OrderBilled message received.");
+            
+            return ProcessOrder(context);
+        }
+
+        private async Task ProcessOrder(IMessageHandlerContext context)
+        {
+                await context.SendLocal(new ShipOrder() { OrderId = Data.OrderId });
+                MarkAsComplete();
+        }
+    }
+}
---
+++ Oefening3_4.7/Shipping/ShippingPolicyData.cs
@@ -0,0 +1,9 @@
+﻿using NServiceBus;
+
+namespace Shipping
+{
+    class ShippingPolicyData : ContainSagaData
+    {
+        public string OrderId { get; set; }
+    }
+}
