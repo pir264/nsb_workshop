924d4565762036eacb881c5d49374ced87e716b7
pir264 <pir264@users.noreply.github.com>
2022-05-19 13:48:02 +0200

    Delete Oefening1/Platform/bin/Debug/netcoreapp3.1 directory

--- Oefening1/Platform/bin/Debug/netcoreapp3.1/platform/servicepulse/js/app.constants.js
+++
@@ -1,6 +0,0 @@
-﻿window.defaultConfig = {
-    default_route: '/dashboard',
-    version: '1.30.0',
-    service_control_url: 'http://localhost:49200/api/',
-    monitoring_urls: ['http://localhost:49204/']
-};
